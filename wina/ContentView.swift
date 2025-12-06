b921a18cbb838da3191e9fd3a236e3529fc664cd
jiwon_park <semanticist0@gmail.com>
2025-12-06 16:04:08 +0900

    Initial commit: iOS WebView tester project

---
+++ wina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wina
+//
+//  Created by 박지원 on 12/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wina/winaApp.swift
@@ -0,0 +1,17 @@
+//
+//  winaApp.swift
+//  wina
+//
+//  Created by 박지원 on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct winaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
